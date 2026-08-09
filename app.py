cd762c9bb3932ce16156702a295180e41e39a068
louzyshot <ramandeep.sidana@gmail.com>
2026-08-09 10:01:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ if image_file and api_key:
                 """
                 
                 response = client.models.generate_content(
-                    model='gemini-2.5-flash',
+                    model='gemini-1.5-flash',
                     contents=[
                         types.Part.from_bytes(data=image_bytes, mime_type=image_file.type),
                         prompt
